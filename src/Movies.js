263357e98642f62b06ca9e165a0d555bfa259498
vijay kumar <308vijaykumar@gmail.com>
2023-04-30 13:47:36 +0530

    initial commit

---
+++ src/Movies.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+import { NavLink } from 'react-router-dom';
+
+const Movies = () => {
+  const { movie } = useGlobalContext();
+  console.log(``, movie);
+  return (
+    <section className='movie-page'>
+      <div className='container grid grid-4-col'>
+        {
+          movie.map((currMovie, index) => {
+            const {imdbID, Title, Poster} = currMovie;
+            return <NavLink to={`movie/${imdbID}`} key={index}>
+              <div className='card'> 
+                <div className='card-info'>
+                  <h2>{Title}</h2>
+                  <img src={Poster} alt={imdbID} />
+                </div>
+              </div>
+            </NavLink>
+          })
+        }
+      </div>
+    </section>
+  )
+}
+
+export default Movies;
---
+++ src/context.js
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect, useState } from "react";
+const AppContext = React.createContext();
+
+
+const API_URL = `https://www.omdbapi.com/?apikey=ab821694&s=titanic`;
+
+const AppProvider = ({children}) => {  
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+    const [isError, setIsError] = useState({show: false, msg: ""});
+
+    const getMovies = async(url) => {
+        try{
+            const res = await fetch(url);
+            const data = await res.json();
+           
+            if(data.Response === "True") {
+                setIsLoading(false);
+                setMovie(data.Search);
+            }
+            else{
+                setIsError(
+                    {
+                        show: true,
+                        msg: data.error
+                    }
+                )
+            }
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        getMovies(API_URL);
+    }, [])
+    return <AppContext.Provider value={{ isLoading, isError, movie }}> {children} </AppContext.Provider>
+}
+
+const useGlobalContext = () =>{
+    return useContext(AppContext);
+}
+
+export { AppContext, AppProvider, useGlobalContext };
